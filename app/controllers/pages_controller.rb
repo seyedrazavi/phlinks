025d4b96fc3fc7d22b603520d7707015a532788e
Seyed Razavi <me@srazavi.com>
2019-09-01 09:03:56 +0100

    About page and admin view mode on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   def home
   	@links = Link.all_but_deleted
+  	@admin_mode = params[:admin]
     respond_to do |format|
       format.html
       format.rss { render :layout => false }
     end
+  end
+
+  def about
   end
 end
