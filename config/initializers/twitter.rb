bd878649e0681dba60dba1a46c664f7667b9d277
Seyed Razavi <me@srazavi.com>
2020-04-19 13:33:46 +0100

    Fixing get list for user

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -7,3 +7,6 @@ TWITTER = Twitter::REST::Client.new do |config|
   config.access_token        = ENV["twitter_access_token"]
   config.access_token_secret = ENV["twitter_access_secret"]
 end
+
+LIST_USER_ID = ENV["list_user_id"]
+LIST_ID = ENV["list_id"]
